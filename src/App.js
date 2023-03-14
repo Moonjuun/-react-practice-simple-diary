738f80940a494ce11f4616762997de12d4a17399
Jun <cmoonjun11@gmail.com>
2023-03-14 14:31:47 +0900

    리액트에서 API 호출하기 (fetch)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   // DiaryEditor, DiaryList가 함께 쓸 일기 data가 있다. 빈 배열로 시작
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0,20).map((it) =>{
+      return {
+        author : it.email,
+        content : it.body,
+        emotion : Math.floor(Math.random()*5)+1,
+        created_date : new Date().getTime(),
+        id : dataId.current++
+      }
+    })
+    setData(initData);
+  };
+
+  // 마운트 되는 시점에 함수를 호출!
+  useEffect(() => {
+    getData();
+  }, [])
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
